2f75de859939437900db919af4ce399a3fdaa7ca
tatiany <tatianysouza2018@gmail.com>
2026-01-16 19:13:33 -0300

    Feat: adicionando tela de listagem de eventos

--- frontend/routes/AppRoutes.tsx
+++ frontend/routes/AppRoutes.tsx
@@ -1,7 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/home/Home';
 import Inspiracional from '../screens/Inspiracional/Inspiracional';
-import CreateInspiration from '../screens/createInspiration/CreateInspiration'
+import CreateInspiration from '../screens/createInspiration/CreateInspiration';
+import Events from '../screens/eventts/Events';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function AppRoutes() {
       <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
       <Stack.Screen options={{headerShown: false}} name="Inspiracional" component={Inspiracional} />
       <Stack.Screen options={{headerShown: false}} name="CriarInspiracional" component={CreateInspiration}/>
+      <Stack.Screen options={{headerShown: false}} name="Eventos" component={Events}/>
     </Stack.Navigator>
   );
 }
